80d82d4cfca4edde9b3b04f2f1fb816b8b4d4fe1
samazon <letcsam@gmail.com>
2020-12-24 02:16:55 +0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const fs = require('fs');
+const hbs = require("hbs");
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+hbs.registerHelper('getCurrentYear', ()=> {
+    return new Date().getFullYear();
+})
+hbs.registerHelper('capitalizeText', (text)=> {
+    return text.toUpperCase();
+})
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'))
+// app.use((req, res, next)=> {
+//     let now = new Date().toString();
+//     let log = `${now}: ${req.method} ${req.url}`;
+
+//     console.log(log)
+//     fs.appendFile('server.log', log + '\n', (err)=> {
+//         if(err){
+//             console.log(err); 
+//         }
+//     })
+//     next();
+// })
+app.get('/', (req,res) => {
+    res.render('home.hbs', {
+        pageName: 'Home Page',
+        welcomeMessage: 'Welcome to the Club'
+    });
+})
+
+app.get('/about', (req,res) => {
+    res.render('about.hbs', {
+        pageName: 'About Page',
+    });
+})
+
+
+app.listen(3000);
